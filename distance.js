6eb0e0a3fdfda9179e1a870f3b2595f9b2bd1b15
Sircule14 <victor.jromar@gmail.com>
2023-10-25 13:27:08 -0600

    Add function that allows add data (kms)

---
+++ distance.js
@@ -0,0 +1,29 @@
+/* A code that allows save the KMs on an array */
+
+
+/* Array that will contain the data of kilometers achived */
+const distanceAchieved = [];
+
+function addDistance (km) {
+    distanceAchieved.push(km);
+}
+
+addDistance(1.96);
+addDistance(1.94);
+addDistance(2);
+addDistance(2.32);
+addDistance(2.2);
+addDistance(2.25);
+addDistance(2.17);
+addDistance(2.71);
+addDistance(2.61);
+addDistance(3.18);
+addDistance(3.24);
+addDistance(3.27);
+addDistance(3.64);
+addDistance(3.39);
+addDistance(3.42);
+addDistance(4.06);
+addDistance(3.91);
+
+console.log(distanceAchieved);
