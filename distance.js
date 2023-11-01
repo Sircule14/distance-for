6284d5b88b899a6614937e61aa178e3f4b8009b9
Sircule14 <victor.jromar@gmail.com>
2023-11-01 12:09:10 -0600

    add function that allows to separate and calculate distance peer week

--- distance.js
+++ distance.js
@@ -26,6 +26,7 @@ addDistance(3.42);
 addDistance(4.06);
 addDistance(3.91);
 addDistance(4.15);
+addDistance(4.77);
 
 
 console.log(distanceAchieved);
@@ -38,6 +39,22 @@ function sumOfKm (array) {
     return sum;
 }
 
-const totalDistance = "Distancia recorrida " + sumOfKm(distanceAchieved);
+const totalDistance = "Distancia total recorrida " + sumOfKm(distanceAchieved);
 
 console.log(totalDistance);
+
+function perWeek(distanceAchieved, week) { //Function that separates and calculate the total of distance by weeks
+    const startIndex = (week - 1) * 3;
+    const weekData = distanceAchieved.slice(startIndex, startIndex + 3);
+    const total = sumOfKm(weekData);
+    
+    return total;
+}
+
+console.log(perWeek(distanceAchieved,1));
+console.log(perWeek(distanceAchieved,2));
+console.log(perWeek(distanceAchieved,3));
+console.log(perWeek(distanceAchieved,4));
+console.log(perWeek(distanceAchieved,5));
+console.log(perWeek(distanceAchieved,6));
+console.log(perWeek(distanceAchieved,7));
